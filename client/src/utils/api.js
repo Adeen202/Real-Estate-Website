e64294014a717a0c0c0d86b7c668ba2c306aa51c
Adeen202 <khawajaaadeen@gmail.com>
2025-09-08 21:21:48 +0500

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import { toast } from 'react-toastify'
 
 export const api = axios.create({
-    baseURL: "https://real-estate-website-nine-ashen.vercel.app/api"
+    baseURL: "https://real-estate-website-server-gamma.vercel.app/api"
 })
 
 export const getAllProperties = async () => {
