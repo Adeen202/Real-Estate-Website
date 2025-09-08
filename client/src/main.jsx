9b727d997ecc01f640bc7794750c75c4616abf0c
Adeen202 <khawajaaadeen@gmail.com>
2025-09-08 18:20:19 +0500

    updated audience

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{ redirect_uri: window.location.origin }}
-      audience="http://localhost:9000"
+      audience={import.meta.env.VITE_AUTH0_AUDIENCE}
       scope="openid profile email"
     >
 
